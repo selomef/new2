1ea72e6b749e84a52afbe3c3ab3b823d697940d8
selomef <90533406+selomef@users.noreply.github.com>
2021-10-14 20:31:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,8 @@ recv4 = clientSocket.recv(1025)
 
    # Send message data.
    # Fill in start
-
+clientSocket.send(msg.encode())
+clientSocket.send(endmsg.encode())
 recv_msg = clientSocket.recv(1025)
 #print(recv_msg.decode())
 #if recv1[:3] != '250':
@@ -87,19 +88,4 @@ message = clientSocket.recv(1025)
 clientSocket.close()
    # Fill in end
 
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
